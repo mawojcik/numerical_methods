eba2f27e9945f035c98aefbe91ac0bd77af82da1
mawojcik <wojcik.maciej.k@gmail.com>
2022-10-17 01:57:54 +0200

    still woring on exporting table to pdf

--- NUM1/lib/NUM1.py
+++ NUM1/lib/NUM1.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import pandas as pd
+
+
 
 # variables
 accuracy = np.float64
@@ -37,7 +40,32 @@ def chart():
 
 
 def print_miscalculation():
-    print('miscalculation')
+    a = miscalculation(derivative_a, point)
+    b = miscalculation(derivative_b, point)
+
+    # for x in range(300):
+        # print("{:.5f}".format(a))
+        # print("{:.10f}".format(a[x]), "{:.10f}".format(b[x]))
+
+
+def generate_table():
+    fig, ax = plt.subplots()
+    ax.axis('off')
+    ax.axis('tight')
+
+    a = miscalculation(derivative_a, point)
+    b = miscalculation(derivative_b, point)
+
+    for x in range(300):
+        h[x] = '%0.15f' % h[x]
+
+    table = pd.DataFrame(data ={'H': h,
+                                'Function a miscalculation': a,
+                                'Function b miscalculation:': b
+                                })
+    ax.table(cellText=table.values[::int(20)], colLabels=table.columns, loc='center')
+    fig.tight_layout()
+    plt.savefig('table.pdf')
 
 
 if len(sys.argv) != 3:
@@ -52,9 +80,12 @@ elif sys.argv[1] == 'double':
 else:
     print('Wrong argument!')
 
+
 if sys.argv[2] == 'chart':
     chart()
 elif sys.argv[2] == 'miscalculation':
-    print_miscalculation()
+    # print(miscalculation(derivative_a, point))
+    # print_miscalculation()
+    generate_table()
 else:
     print('Wrong argument!')
