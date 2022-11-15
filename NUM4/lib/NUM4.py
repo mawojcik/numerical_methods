d00f51d75454b5f709de03097f564c2b62f79323
mawojcik <wojcik.maciej.k@gmail.com>
2022-11-15 22:22:43 +0100

    program is done, report with chart and 3. Sherman-Morrison to do

--- NUM4/lib/NUM4.py
+++ NUM4/lib/NUM4.py
@@ -1,31 +1,35 @@
 import numpy as np
+import time
+import matplotlib.pyplot as plt  # Biblioteka do tworzenia wykresow
 
-array_size = 50;
+array_size = 50
 b = [5] * array_size
 
 
-# A = B + a*a^T
 def solve_using_sherman():
     A = [[9] * array_size, [7] * (array_size - 1) + [0]]
+    start = time.time()
 
-    # Backward subtitution dla obu równań
+    # backward substitution
     z = [0] * array_size
     x = [0] * array_size
     z[array_size - 1] = b[array_size - 1] / A[0][array_size - 1]
     x[array_size - 1] = 1 / A[0][array_size - 1]
 
     for i in range(array_size - 2, -1, -1):
-        z[i] = (b[array_size - 2] - A[1][i] * z[i + 1]) / A[0][i]
-        x[i] = (1 - A[1][i] * x[i + 1]) / A[0][i]
+        z[i] = (b[array_size - 2] - z[i + 1] * A[1][i]) / A[0][i]
+        x[i] = (1 - x[i + 1] * A[1][i]) / A[0][i]
 
-    delta = sum(z) / (1 + sum(x))
+    delta = sum(z) / (sum(x) + 1)
 
-    # Wyliczenie wyniku
     y = []
     for i in range(len(z)):
         y.append(z[i] - x[i] * delta)
 
     print(y)
+
+    return time.time()-start
+
 
 
 def solve_using_numpy():
@@ -36,8 +40,10 @@ def solve_using_numpy():
                 A[i][i] = 10
                 if j > 0:
                     A[i - 1][j] = 8
+    start = time.time()
+    print(np.linalg.solve(A, b))
+    return time.time()-start
+
 
 
 solve_using_sherman();
-
-
