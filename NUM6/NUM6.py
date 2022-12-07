507f8c763a99ad29bc4d8d44ff91c03b644977bc
mawojcik <wojcik.maciej.k@gmail.com>
2022-12-07 23:39:51 +0100

    final version

--- NUM6/NUM6.py
+++ NUM6/NUM6.py
@@ -12,19 +12,19 @@ matrix_B = np.array([[3, 4, 2, 4],
 precision = 10 ** -10
 
 
-def qr_iteration(matrix):
-    Q, R = np.linalg.qr(matrix)
-    return np.matmul(R, Q)
-
-
 def qr_approximation(matrix):
     value = []
-    for i in range(1, len(matrix)):
-        value.append(abs(matrix[i][i - 1]))
+    for i in range(0, len(matrix) - 1):
+        value.append(abs(matrix[i + 1][i]))
     for i in value:
         if i > precision:
             return True
     return False
+
+
+def qr_iteration(matrix):
+    Q, R = np.linalg.qr(matrix)
+    return np.matmul(R, Q)
 
 
 def qr_eigenvalues(matrix):
@@ -34,37 +34,37 @@ def qr_eigenvalues(matrix):
     return eigenvalues
 
 
+# QR
 matrix_M_as_QR = qr_iteration(matrix_M)
 while qr_approximation(matrix_M_as_QR):
     matrix_M_as_QR = qr_iteration(matrix_M_as_QR)
 
-print(qr_eigenvalues(matrix_M_as_QR))  # podpunkt a
-print(np.linalg.eig(matrix_M)[0])  # test a
-
-
-def power_iteration(matrix, vector):
-    x = np.matmul(matrix, vector)
-    return x / np.linalg.norm(x)
+print("Iteration eigenvalues: " + str(qr_eigenvalues(matrix_M_as_QR)))  # podpunkt a
+print("NumPy eigenvalues: " + str(np.linalg.eig(matrix_M)[0]))  # test a
 
 
 def power_approximation(a_vec, b_vec):
     return np.linalg.norm(a_vec - b_vec) > precision
 
 
+def power_iteration(matrix, vector):
+    z = np.matmul(matrix, vector)
+    return z / np.linalg.norm(z)
+
+
 def power_eigenvalue(matrix, vector):
     return np.matmul(matrix, vector)[0] / vector[0]
 
 
+# potegowa
 corresponding_vector = []
 for i in range(len(matrix_B)):
     corresponding_vector.append(1)
 
-temporary_vector = power_iteration(matrix_B, corresponding_vector)
-while power_approximation(temporary_vector, corresponding_vector):
-    corresponding_vector = temporary_vector
-    temporary_vector = power_iteration(matrix_B, corresponding_vector)
+z = power_iteration(matrix_B, corresponding_vector)
+while power_approximation(z, corresponding_vector):
+    corresponding_vector = z
+    z = power_iteration(matrix_B, corresponding_vector)
 
-print(power_eigenvalue(matrix_B, corresponding_vector)) # podpunkt b
-print(corresponding_vector) # podpunkt b
-
-# Do zrobienia: znalezc wektory wlasne
+print("Max eigenvalue: " + str(power_eigenvalue(matrix_B, corresponding_vector)))  # podpunkt b
+print("Corresponding vector: " + str(corresponding_vector))  # podpunkt b
