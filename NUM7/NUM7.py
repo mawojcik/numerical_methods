239e93df8c7372da0907b9c5b5857295eb72b910
mawojcik <wojcik.maciej.k@gmail.com>
2022-12-18 14:01:35 +0100

    final final version

--- NUM7/NUM7.py
+++ NUM7/NUM7.py
@@ -14,12 +14,13 @@ def b_node(n):
 def y_function(x):
     return 1 / (25 * (x**2) + 1)
 
+
 def fun2(x):
     return 1 / (3 * x**4 + 3)
 
 
 def fun3(x):
-    return x**2 / (1 + 2 * x**6)
+    return x**4 / (1 + 2 * x**6)
 
 
 def interpolation(fun, x_range, n, node):
@@ -50,7 +51,7 @@ def plot():
 
     axs[0, 0].grid()
     axs[0, 0].set_title(
-        'Wielomiany interpolacyjne dla\n' r'$f(x)=\frac{1}{1+25x^2}$ i $x_i=-1+2\frac{i}{n}$')
+        'Wielomiany interpolacyjne dla\n' r'$y(x)=\frac{1}{1+25x^2}$ i $x_i=-1+2\frac{i}{n}$')
     axs[0, 0].plot(x_range, y_function(x_range), 'c', label='y(x)')
     axs[0, 0].plot(x_range, interpolation(y_function, x_range, 2, a_node), 'b', label=r'$W_2(x)$')
     axs[0, 0].plot(x_range, interpolation(y_function, x_range, 9, a_node), 'g', label=r'$W_9(x)$')
@@ -59,7 +60,7 @@ def plot():
 
     axs[0, 1].grid()
     axs[0, 1].set_title(
-        'Wielomiany interpolacyjne dla\n' r'$f(x)=\frac{1}{1+25x^2}$ i $x_i=\cos(\pi\frac{2i+1}{2(n+1)})$')
+        'Wielomiany interpolacyjne dla\n' r'$y(x)=\frac{1}{1+25x^2}$ i $x_i=\cos(\pi\frac{2i+1}{2(n+1)})$')
     axs[0, 1].plot(x_range, y_function(x_range), 'c', label='y(x)')
     axs[0, 1].plot(x_range, interpolation(y_function, x_range, 2, b_node), 'b', label=r'$W_2(x)$')
     axs[0, 1].plot(x_range, interpolation(y_function, x_range, 9, b_node), 'g', label=r'$W_9(x)$')
@@ -68,7 +69,7 @@ def plot():
 
     axs[1, 0].grid()
     axs[1, 0].set_title(
-        'Wielomiany interpolacyjne dla\n' r'$f(x)=\frac{1}{1+x^2}$ i $x_i=-1+2\frac{i}{n}$')
+        'Wielomiany interpolacyjne dla\n' r'$y(x)=\frac{1}{3x^4+3}$ i $x_i=-1+2\frac{i}{n}$')
     axs[1, 0].plot(x_range, fun2(x_range), 'c', label='y(x)')
     axs[1, 0].plot(x_range, interpolation(fun2, x_range, 2, a_node), 'b', label=r'$W_2(x)$')
     axs[1, 0].plot(x_range, interpolation(fun2, x_range, 3, a_node), 'g', label=r'$W_3(x)$')
@@ -77,7 +78,7 @@ def plot():
 
     axs[1, 1].grid()
     axs[1, 1].set_title(
-        'Wielomiany interpolacyjne dla\n' r'$f(x)=\frac{1}{1+x^2}$ i $x_i=\cos(\pi\frac{2i+1}{2(n+1)})$')
+        'Wielomiany interpolacyjne dla\n' r'$y(x)=\frac{1}{3x^4+3}$ i $x_i=\cos(\pi\frac{2i+1}{2(n+1)})$')
     axs[1, 1].plot(x_range, fun2(x_range), 'c', label='y(x)')
     axs[1, 1].plot(x_range, interpolation(fun2, x_range, 2, b_node), 'b', label=r'$W_2(x)$')
     axs[1, 1].plot(x_range, interpolation(fun2, x_range, 30, b_node), 'g', label=r'$W_{30}(x)$')
@@ -86,44 +87,44 @@ def plot():
 
     axs[2, 0].grid()
     axs[2, 0].set_title(
-        'Wielomiany interpolacyjne dla\n' r'$f(x)=\frac{1}{1+x^2}$ i $x_i=-1+2\frac{i}{n}$')
+        'Wielomiany interpolacyjne dla\n' r'$y(x)=\frac{x^4}{1+2x^6}$ i $x_i=-1+2\frac{i}{n}$')
     axs[2, 0].plot(x_range, fun3(x_range), 'c', label='y(x)')
     axs[2, 0].plot(x_range, interpolation(fun3, x_range, 2, a_node), 'b', label=r'$W_2(x)$')
     axs[2, 0].plot(x_range, interpolation(fun3, x_range, 5, a_node), 'g', label=r'$W_5(x)$')
-    axs[2, 0].plot(x_range, interpolation(fun3, x_range, 7, a_node), 'm', label=r'$W_{7}(x)$')
+    axs[2, 0].plot(x_range, interpolation(fun3, x_range, 60, a_node), 'm', label=r'$W_{60}(x)$')
     axs[2, 0].legend(loc=9, prop={'size': 9})
 
     axs[2, 1].grid()
     axs[2, 1].set_title(
-        'Wielomiany interpolacyjne dla\n' r'$f(x)=\frac{1}{1+x^2}$ i $x_i=\cos(\pi\frac{2i+1}{2(n+1)})$')
-    axs[2, 1].plot(x_range, fun2(x_range), 'r', label='y(x)')
+        'Wielomiany interpolacyjne dla\n' r'$y(x)=\frac{x^4}{1+2x^6}$ i $x_i=\cos(\pi\frac{2i+1}{2(n+1)})$')
+    axs[2, 1].plot(x_range, fun3(x_range), 'c', label='y(x)')
     axs[2, 1].plot(x_range, interpolation(fun3, x_range, 2, b_node), 'b', label=r'$W_2(x)$')
-    axs[2, 1].plot(x_range, interpolation(fun3, x_range, 5, b_node), 'g', label=r'$W_{5}(x)$')
-    axs[2, 1].plot(x_range, interpolation(fun3, x_range, 7, b_node), 'm', label=r'$W_{7}(x)$')
+    axs[2, 1].plot(x_range, interpolation(fun3, x_range, 7, b_node), 'g', label=r'$W_{7}(x)$')
+    axs[2, 1].plot(x_range, interpolation(fun3, x_range, 90, b_node), 'm', label=r'$W_{90}(x)$')
     axs[2, 1].legend(loc=9, prop={'size': 10})
 
     fig.tight_layout(pad=3.0)
     fig.savefig(
         "lewy_gorny.pdf",
-        bbox_inches=mtransforms.Bbox([[0, 0.7], [0.5, 1.05]]).transformed(
+        bbox_inches=mtransforms.Bbox([[0, 0.65], [0.5, 0.96]]).transformed(
             fig.transFigure - fig.dpi_scale_trans
         )
     )
     fig.savefig(
         "prawy_gorny.pdf",
-        bbox_inches=mtransforms.Bbox([[0.5, 0.7], [1, 1.05]]).transformed(
+        bbox_inches=mtransforms.Bbox([[0.5, 0.65], [1, 0.96]]).transformed(
             fig.transFigure - fig.dpi_scale_trans
         )
     )
     fig.savefig(
         "lewy_srodkowy.pdf",
-        bbox_inches=mtransforms.Bbox([[0, 0.35], [0.5, 0.7]]).transformed(
+        bbox_inches=mtransforms.Bbox([[0, 0.35], [0.5, 0.65]]).transformed(
             fig.transFigure - fig.dpi_scale_trans
         )
     )
     fig.savefig(
         "prawy_srodkowy.pdf",
-        bbox_inches=mtransforms.Bbox([[0.5, 0.35], [1, 0.7]]).transformed(
+        bbox_inches=mtransforms.Bbox([[0.5, 0.35], [1, 0.65]]).transformed(
             fig.transFigure - fig.dpi_scale_trans
         )
     )
