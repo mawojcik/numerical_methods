bf238d3721ceb8398d837af1c037683e20798dd0
mawojcik <wojcik.maciej.k@gmail.com>
2023-01-04 00:49:02 +0100

    b function repaired

--- NUM8/NUM8.py
+++ NUM8/NUM8.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 FILE_NAME = 'data.txt'
 X = sympy.Symbol('x')
 FUNCTION_A_COMPONENTS = [sympy.sin(2*X), sympy.sin(3*X), sympy.cos(5*X), sympy.exp(-X)]
-FUNCTION_B_COMPONENTS = [sympy.sin(X), sympy.cos(X)]
+FUNCTION_B_COMPONENTS = [sympy.sin(2*X), sympy.cos(3*X)]
 FUNCTION_B_COEFFICIENTS = [2, 3]
 FUNCTION_B_XPOINTS = np.linspace(0, 10, 100)
 
@@ -28,27 +28,16 @@ def _evaluate_function(function_components: np.ndarray, function_coefficients: n
     return np.array([function.evalf(subs={X: arg}) for arg in x_args])
 
 
-def _get_coefficients_using_least_squares_method(given_points: np.ndarray, function_components: np.ndarray)\
-        -> np.ndarray:
-    """
-    The function takes a list of points and the function itself with unknown coefficients at each component,
-    and based on the least squares method, it determines approximate values of the coefficients.
-
-    :param given_points: list of points based on which we will perform interpolation using the least squares method
-    :param function_components: Our function in the form of a list of components
-    :return: Vector with coefficients next to every component
-    """
-    b_vector = given_points[:, 1]   # b_vector to są wartości y
-    #TODO: column stack to for
-    matrix_A = np.column_stack(list(map(lambda f: [f.evalf(subs={X: x_value}) for x_value in given_points[:, 0]],
-                                        function_components))).astype(np.double)
-    return np.linalg.inv(matrix_A.T @ matrix_A) @ matrix_A.T @ given_points[:, 1]
+def get_coefficients_using_least_squares_method(given_points, function_components):
+    y_values = [y for x, y in given_points]
+    matrix_A = np.column_stack(list(map(lambda f: [f.evalf(subs={X: x_value}) for x_value in given_points[:, 0]], function_components))).astype(np.double)
+    return np.linalg.inv(matrix_A.T @ matrix_A) @ matrix_A.T @ y_values
 
 
 def a_function(points_to_approximate, function_components):
-    coefficients = _get_coefficients_using_least_squares_method(points_to_approximate, function_components)
+    coefficients = get_coefficients_using_least_squares_method(points_to_approximate, function_components)
     dense_x_range = np.arange(points_to_approximate[:, 0].min(), points_to_approximate[:, 0].max() + 0.01, 0.01)
-    plt.figure(figsize=(13, 7))
+    plt.figure(figsize=(13, 8))
     plt.grid(True)
     plt.title("Aproksymacja przy użyciu metody najmniejszych kwadratów funkcji:"
               "f(x) = a*sin(2x) + b*sin(3x) +c*cos(5x) + d*exp(-x)")
@@ -58,34 +47,34 @@ def a_function(points_to_approximate, function_components):
     plt.plot(dense_x_range, evaluate_function(function_components, coefficients, dense_x_range), color='red', label='Approximated function')
     plt.legend(loc=0)
     plt.show()
+    # @TODO: save plot
 
 
 def b_function(points_to_approximate, function_components, exact_coefficients):
-    coefficients = _get_coefficients_using_least_squares_method(points_to_approximate, function_components)
+    coefficients = get_coefficients_using_least_squares_method(points_to_approximate, function_components)
     dense_x_range = np.arange(points_to_approximate[:, 0].min(), points_to_approximate[:, 0].max() + 0.01, 0.01)
 
-    plt.figure(figsize=(10, 7))
+    plt.figure(figsize=(13, 8))
     plt.xlabel("x"), plt.ylabel("y")
     plt.grid(True)
-    plt.title("Function approximation using least squares method\n$F(x)=" +
-              "+".join([f"{chr(ord('a') + i)}*{c}" for i, c in enumerate(function_components)]) + r"$")
-
-    print("Function = " + "+".join([f"{chr(ord('a') + i)}*{c}" for i, c in enumerate(function_components)]))
-    print(f"Exact coefficients = {exact_coefficients}")
+    plt.title("Aproksymacja przy użyciu metody najmniejszych kwadratów funkcji:"
+              "g(x) = ")
+    print("g(x) = ")
+    print("Exact coefficients = ", exact_coefficients)
     print("Found coefficients = ", coefficients)
     plt.plot(dense_x_range, evaluate_function(function_components, exact_coefficients, dense_x_range), color='black', lw=3)
     plt.plot(dense_x_range, evaluate_function(function_components, coefficients, dense_x_range), color='red')
     plt.show()
+    # @TODO: save plot
 
 
 if sys.argv[1] == 'exercise_a':
     a_function(np.loadtxt(FILE_NAME), FUNCTION_A_COMPONENTS)
 elif sys.argv[1] == 'exercise_b':
     # TODO: twórz punkty w funkcji, czyli points w srodku funkcji
-    yValues = evaluate_function(np.array(list(map(sympy.sympify, FUNCTION_A_COMPONENTS))), FUNCTION_B_COEFFICIENTS, FUNCTION_B_XPOINTS)
+    yValues = evaluate_function(np.array(list(map(sympy.sympify, FUNCTION_B_COMPONENTS))), FUNCTION_B_COEFFICIENTS, FUNCTION_B_XPOINTS)
     for a in range(len(yValues)):
         yValues[a] = yValues[a] + np.random.random()
-    # yValues = yValues + np.random.random()
-    # yValues = yValues + np.random.normal(0, NOISE_SCALE, len(yValues))
     points = np.array([[x, y] for x, y in zip(FUNCTION_B_XPOINTS, yValues)], dtype=np.double)
     b_function(points, FUNCTION_B_COMPONENTS, FUNCTION_B_COEFFICIENTS)
+
