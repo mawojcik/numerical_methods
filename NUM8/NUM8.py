5ebd2c274f3016115c1e4c090e67a476df963efb
mawojcik <wojcik.maciej.k@gmail.com>
2023-01-04 18:16:48 +0100

    changed plotting

--- NUM8/NUM8.py
+++ NUM8/NUM8.py
@@ -15,8 +15,8 @@ def a_function(points_to_approximate):
 
     plt.figure(figsize=(13, 8))
     plt.grid(True)
-    plt.title("Aproksymacja przy użyciu metody najmniejszych kwadratów funkcji:"
-              "f(x) = a*sin(2x) + b*sin(3x) +c*cos(5x) + d*exp(-x)")
+    plt.title("Aproksymacja przy użyciu metody najmniejszych kwadratów funkcji:\n"
+              "f(x) = a*sin(2x) + b*sin(3x) +c*cos(5x) + d*exp(-x)", fontsize=20)
     print("f(x) = a*sin(2x) + b*sin(3x) +c*cos(5x) + d*exp(-x)")
     print("Znalezione wspolczynniki = ", coefficients)
 
@@ -24,7 +24,7 @@ def a_function(points_to_approximate):
     y_values = coefficients[0] * np.sin(2 * denser_x_points) + coefficients[1] * np.sin(3 * denser_x_points) + coefficients[2] * np.cos(5 * denser_x_points) + coefficients[3] * np.exp(-denser_x_points)
     plt.plot(x_points, y_points, 'o', color='black', label='Dokładne punkty')
     plt.plot(denser_x_points, y_values, color='red', label='Aproksymowana funkcja')
-    plt.legend(loc=0)
+    plt.legend(loc=0, fontsize = 15)
     plt.savefig("a.pdf")
     plt.show()
 
@@ -42,8 +42,8 @@ def b_function(exact_coefficients):
 
     plt.figure(figsize=(13, 8))
     plt.grid(True)
-    plt.title("Aproksymacja przy użyciu metody najmniejszych kwadratów funkcji:"
-              "g(x) = a*2x + b*cos(3x) +c*cos(5x)")
+    plt.title("Aproksymacja przy użyciu metody najmniejszych kwadratów funkcji:\n"
+              "g(x) = a*2x + b*cos(3x) +c*cos(5x)", fontsize=20)
     print("g(x) = a*cos(2x) + b*cos(3x) +c*cos(5x)")
     print("Dokladne wspolczynniki = ", exact_coefficients)
     print("Znalezione wspolczynniki = ", coefficients)
@@ -52,7 +52,7 @@ def b_function(exact_coefficients):
     y_values = exact_coefficients[0] * 2 * denser_x_points + exact_coefficients[1] * np.cos(3 * denser_x_points) + exact_coefficients[2] * np.cos(5 * denser_x_points)
     plt.plot(x_points, y_disturbed, 'o', color='black', label='Zaburzone punkty')
     plt.plot(denser_x_points, y_values, color='red', label='Dokładna funkcja')
-    plt.legend(loc=0)
+    plt.legend(loc=0, fontsize = 15)
     plt.savefig("b.pdf")
     plt.show()
 
